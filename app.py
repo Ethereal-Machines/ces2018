1eaa37fe9c716ffea7ac5489c0914227edcad2c4
Vivek Anand <vivekanand1101@gmail.com>
2018-01-08 18:06:53 +0530

    Basic backend with captcha
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 # coding=utf-8
+import os
+
 import flask
 from flask import Flask
+from flask_wtf.csrf import CsrfProtect
 
 APP = Flask(__name__)
+APP.config.from_object('config')
 
-@APP.route("/")
+if os.environ.get('STORE_CONFIG') is not None:
+    APP.config.from_envvar('STORE_CONFIG')
+CsrfProtect(app=APP)
+
+import model
+from model import engine, Base
+import forms
+
+SESSION = model.create_session(APP.config['DB_URL'], engine, Base)
+
+@APP.route("/", methods=['GET'])
 def index():
     ''' Give out the form for preordering '''
-    return flask.render_template('index.html')
+    form = forms.DiscountForm()
+    return flask.render_template('index.html', form=form)
+
+
+@APP.route("/getcoupon", methods=['POST'])
+@APP.route("/getcoupon/", methods=['POST'])
+def get_coupon():
+    ''' Verify the form and register the user for getting coupon '''
+    form = forms.DiscountForm()
+    if form.validate_on_submit():
+        # add to the database
+        print 'coming here bro'
+        return "land raaj"
+    for fieldName, errorMessages in form.errors.items():
+        print "field: %s, error: %s" % (fieldName, errorMessages)
+    return 'lauda'
