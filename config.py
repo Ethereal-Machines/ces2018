3963ceabf6f9fdb76ff2d95cca8ebb2d34b21725
Vivek Anand <vivekanand1101@gmail.com>
2018-01-09 13:32:49 +0530

    Add config for sendgrid, logging
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
 DB_URL = 'postgresql://postgres:@localhost/ces'
 DEBUG = True
+SECRET_KEY = 'secret achha waala'
+
+# captcha
 RECAPTCHA_USE_SSL = True
-RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
-RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
-SECRET_KEY = 'secret'
+RECAPTCHA_PUBLIC_KEY = 'heheheheheheh'
+RECAPTCHA_PRIVATE_KEY = 'lolololololololoolol'
+
+# Sendgrid
+SENDGRID_API_KEY = 'your api key'
+SENDGRID_DEFAULT_FROM = 'admin@yourdomain.com'
+EMAIL_BODY = '<p>Ye lo %s, discount code: %s </p>'
+
+#logging
+LOG_FILE = 'store.log'
